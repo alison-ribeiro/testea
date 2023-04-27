b798c6d021b8e03877224222cf683b9999d17883
Alison R Alves <88009326+alison-ribeiro@users.noreply.github.com>
2023-04-26 22:01:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
+import plotly.express as px
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+df = px.data.iris()
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
-    html.H1(children='Gapminder Data'),
+    html.H1(children='Iris Data'),
 
     html.Div(children='''
         Dash: A web application framework for Python.
@@ -18,42 +18,17 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': df[df['year']==year]['gdpPercap'], 'y': df[df['year']==year]['lifeExp'], 'type': 'scatter', 'text': df[df['year']==year]['country'], 'mode': 'markers'},
+                {'x': df[df['species']==species]['sepal_width'], 'y': df[df['species']==species]['sepal_length'], 'type': 'scatter', 'mode': 'markers', 'name': species} for species in df['species'].unique()
             ],
             'layout': {
-                'title': 'Gapminder Visualization',
-                'xaxis': {'title': 'GDP per capita'},
-                'yaxis': {'title': 'Life Expectancy'},
+                'title': 'Iris Visualization',
+                'xaxis': {'title': 'Sepal Width'},
+                'yaxis': {'title': 'Sepal Length'},
                 'hovermode': 'closest'
             }
         }
-    ),
-
-    dcc.Slider(
-        id='year-slider',
-        min=df['year'].min(),
-        max=df['year'].max(),
-        value=df['year'].min(),
-        marks={str(year): str(year) for year in df['year'].unique()},
-        step=None
     )
 ])
 
-@app.callback(
-    dash.dependencies.Output('example-graph', 'figure'),
-    [dash.dependencies.Input('year-slider', 'value')])
-def update_figure(selected_year):
-    filtered_df = df[df['year'] == selected_year]
-    fig = {
-        'data': [
-            {'x': filtered_df['gdpPercap'], 'y': filtered_df['lifeExp'], 'type': 'scatter', 'text': filtered_df['country'], 'mode': 'markers'}
-        ],
-        'layout': {
-            'title': 'Gapminder Visualization',
-            'xaxis': {'title': 'GDP per capita'},
-            'yaxis': {'title': 'Life Expectancy'},
-            'hovermode': 'closest'
-        }
-    }
-
-    return fig
+if __name__ == '__main__':
+    app.run_server(debug=True)
